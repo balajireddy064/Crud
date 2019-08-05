dbc8b412f8d2e75cd4f1bf15f50e6c198fbba327
balajireddy064 <52796649+balajireddy064@users.noreply.github.com>
2019-08-05 15:18:46 +0530

    ExampleDemoPractice
    Exception class

--- PractiseDemo/src/main/java/com/example/demo/customeException/ExampleCustumException.java
+++ PractiseDemo/src/main/java/com/example/demo/customeException/ExampleCustumException.java
@@ -3,6 +3,7 @@ package com.example.demo.customeException;
 public class ExampleCustumException extends Exception {
 
 	public ExampleCustumException(String message) {
+		System.out.println("well come to exception");
 		super(message);
 	}
 
